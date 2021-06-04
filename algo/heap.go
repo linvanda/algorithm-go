1b5dbaae37cc8c21ee2903ed1f9d19a4edbfb1ef
songlin.zhang <songlin.zhang@weicheche.cn>
2021-06-04 21:58:53 +0800

    heap

--- algo/heap.go
+++ algo/heap.go
@@ -10,33 +10,127 @@ package algo
 // 完全二叉树可以用数组表示
 // 此处实现大顶堆
 
-// 建堆
+type Heap struct {
+	lst []int
+	isMax bool
+}
+
+// heap 原始切片
+func(h Heap) List() []int {
+	return h.lst
+}
+
+// 堆大小
+func(h Heap) Size() int  {
+	return len(h.lst)
+}
+
+func(h Heap) IsEmpty() bool  {
+	return len(h.lst) > 0
+}
+
+// 优先级队列：出列
+// 取堆顶元素，然后将堆尾元素放到堆顶，并 heapify
+func(h *Heap) Pop() int {
+	size := len(h.lst)
+	if size == 0 {
+		panic("pop from empty heap")
+	}
+
+	newSize := size - 1
+	// 取出堆顶元素
+	rst := h.lst[0]
+	// 将堆尾元素放到堆顶
+	h.lst[0] = h.lst[size - 1]
+	// 对切片做截取
+	h.lst = h.lst[0:newSize:cap(h.lst)]
+	// heapify
+	heapifiy(h.lst, 0, newSize, h.isMax)
+
+	return rst
+}
+
+// 优先级队列：入列
+// 将元素放到堆尾，然后往上比较
+func(h *Heap) Add(item int) {
+	lst := h.lst
+	lst = append(lst, item)
+
+	for i := len(lst) - 1; i > 0; {
+		var p int
+		if i & 1 == 0 {
+			// 偶数，说明是右节点
+			p = i/2 - 1
+		} else {
+			p = i/2
+		}
+
+		if !compare(lst, p, i, h.isMax) {
+			// 需要和父节点交换
+			lst[p], lst[i] = lst[i], lst[p]
+			// 继续处理父节点
+			i = p
+		} else {
+			// 符合堆性质，结束
+			break
+		}
+	}
+
+	h.lst = lst
+}
+
+// 构建最大堆
 // 时间复杂度：O(n)
-func MaxHeap(lst []int) {
+func NewMaxHeap(lst []int) Heap {
+	return newHeap(lst, true)
+}
+
+// 构建最小堆
+func NewMinHeap(lst []int) Heap {
+	return newHeap(lst, false)
+}
+
+func newHeap(lst []int, isMax bool) Heap {
 	l := len(lst)
 	// 从 i 开始，从下往上堆化，保证在处理第 i 个元素时，大于 i 的所有元素都已经完成堆化
 	// 这里是个循环不变式：在下一次循环前， 所生成的是一个堆，下一次循环会在此基础上生成新的堆
 	for i := l/2 - 1; i >= 0; i-- {
-		maxHeapifiy(lst, i, l)
+		heapifiy(lst, i, l, isMax)
 	}
+
+	return Heap{lst, isMax}
 }
 
-// 堆排序
+// 堆排序（升序）
 // 原址排序
 // 时间复杂度：O(nlgn)
 // 首先将 lst 堆化。堆化完毕后，堆顶一定是最大元素，然后将堆顶元素和最后元素交换位置，并重新堆化。如此反复，直到堆大小变成 2
 func HeapSort(lst []int) {
+	heapSort(lst, true)
+}
+
+// 堆排序（降序）
+func HeapRSort(lst []int) {
+	heapSort(lst, false)
+}
+
+func heapSort(lst []int, asc bool) {
 	if len(lst) < 2 {
 		return
 	}
 
-	MaxHeap(lst)
+	if asc {
+		// 升序
+		NewMaxHeap(lst)
+	} else {
+		NewMinHeap(lst)
+	}
 
 	for i := len(lst) - 1; i > 1; i-- {
 		// 交换 lst[0] 和 lst[i]，使得 i 位置的元素成为当前堆中的最大元素
 		lst[0], lst[i] = lst[i], lst[0]
 		// 对新堆做堆化处理
-		maxHeapifiy(lst, 0, i)
+		heapifiy(lst, 0, i, asc)
 	}
 
 	// 将最后 2 元素的堆元素互换即可
@@ -46,17 +140,17 @@ func HeapSort(lst []int) {
 // 对第 i 个元素堆化
 // 前提：在对第 i 个元素堆化时，大于 i 的元素已经完成了堆化（其子元素子树）
 // 注意：i 是数组下表，数组下标从 0 开始（有些地方为了方便计算是用 1 开始的）
-func maxHeapifiy(lst []int, i int, hSize int) {
+func heapifiy(lst []int, i int, hSize int, isMax bool) {
 	l := i * 2 + 1
 	r := l + 1
 	max := hSize - 1
 	largest := i
 
-	if l <= max && lst[largest] < lst[l] {
+	if l <= max && !compare(lst, largest, l, isMax) {
 		largest = l
 	}
 
-	if r <= max && lst[largest] < lst[r] {
+	if r <= max && !compare(lst, largest, r, isMax) {
 		largest = r
 	}
 
@@ -65,6 +159,14 @@ func maxHeapifiy(lst []int, i int, hSize int) {
 		lst[largest], lst[i] = lst[i], lst[largest]
 
 		// 交换后，需要对被交换的子节点继续做 heapify（因为该交换可能破坏了子节点的堆性质）
-		maxHeapifiy(lst, largest, hSize)
+		heapifiy(lst, largest, hSize, isMax)
+	}
+}
+
+func compare(lst []int, p int, c int, isMax bool) bool {
+	if isMax {
+		return lst[p] >= lst[c]
+	} else {
+		return lst[p] <= lst[c]
 	}
 }
--- main.go
+++ main.go
@@ -14,7 +14,16 @@ func main() {
 	lst3 := []int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7}
 	sub := algo.MaxSubArray(lst3)
 	fmt.Printf("max sub array:%v\n", sub)
-	lst4 := []int{4,2,6,5,9,1,2}
-	algo.HeapSort(lst4)
-	fmt.Printf("heap sort:%v\n", lst4)
+	lst4 := []int{4,2,6,5,9,1,2, 13}
+	heap := algo.NewMaxHeap(lst4)
+	size := heap.Size()
+	for i := 0; i < size; i++ {
+		v := heap.Pop()
+		fmt.Printf("heap pop:%v\n", v)
+	}
+	heap.Add(3)
+	heap.Add(1)
+
+	fmt.Printf("heap pop2:%v\n", heap.Pop())
+	fmt.Printf("heap pop2:%v\n", heap.Pop())
 }
